9d5e5c017a5c971d6184dd3c77b0071682ed4172
unknown <vanlocbk1996@gmail.com>
2019-08-16 21:23:53 +0700

    change color key of dictionary by number instead of word when plotting

--- kmeans.py
+++ kmeans.py
@@ -16,13 +16,13 @@ def read_data(root, filename):
     return X, y
 
 def plot(X, y, centroids):
-    COLORS = {'Iris-setosa':'r', 'Iris-versicolor': 'g', 'Iris-virginica': 'b'}
+    COLORS = {0:'r', 1: 'g', 2: 'b'}
     fig, ax = plt.subplots()
     for i in range(len(X['SepalLengthCm'])):
         ax.scatter(X['SepalLengthCm'][i], X['SepalWidthCm'][i], color=COLORS[y[i]]) 
-    ax.set_title('Iris dataset')
-    ax.set_xlabel('SepalLengthCm')  
-    ax.set_ylabel('SepalWidthCm')
+    # ax.set_title('Iris dataset')
+    # ax.set_xlabel('SepalLengthCm')  
+    # ax.set_ylabel('SepalWidthCm')
 
     _color = ''
     _marker = ''
@@ -84,15 +84,7 @@ y = enecoder.fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 centroids = fit(X_train.values, 3)
 plot(X, y, centroids)
-print(centroids)
+# print(centroids)
 # predict = predict(centroids, X_test.values)
 # print(y.shape)
-# print(np.sum(np.where(predict == y_test)))
-
-from sklearn.cluster import KMeans
-
-
-kmeans = KMeans(n_clusters=3, random_state=0).fit(X_train.values)
-print(kmeans.cluster_centers_)
-# predict = kmeans.predict(X_test)
 # print(np.sum(np.where(predict == y_test)))
